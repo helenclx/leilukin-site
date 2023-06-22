e9123004d8c45832cf3546958ef14f28a2e788af
Leilukin <contact@leilukin.com>
2023-06-23 02:05:23 +0800

    Rewrote the layout of the entire website

---
+++ components/footer.js
@@ -0,0 +1,15 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                Site Launched: September 11, 2022 by Leilukin | <a href="/changelog">Site Changelog</a> | <a href="#page-top">Back to top</a>
+            </footer>
+        `;
+    }
+}
+  
+customElements.define('footer-component', Footer);  
---
+++ shrines/asummersend/components/sidebar-content.js
@@ -0,0 +1,21 @@
+class RightSidebarContent extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        this.innerHTML = `
+            <h2>About</h2>
+            <p><em>A Summer’s End — Hong Kong 1986</em> is a visual novel set in 1980s Hong Kong. It tells of a love story between two women. Lush and vibrant visuals, the game features over 400 unique hand-drawn artwork inspired by Asian cinema and 1980s anime.</p>
+            <h3>Links</h3>
+            <ul>
+                <li><a href="https://www.asummersend.com/" target="_blank">Official Website</a>
+                </li>
+                <li><a href="https://store.steampowered.com/app/1111370/A_Summers_End__Hong_Kong_1986/" target="_blank">Steam</a></li>
+                <li><a href="https://oracleandbone.itch.io/a-summers-end" target="_blank">itch.io</a></li>
+            </ul>
+        `;
+    }
+}
+  
+customElements.define('right-sidebar-component', RightSidebarContent);
