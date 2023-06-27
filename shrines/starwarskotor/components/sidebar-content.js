8e1c938dbec405151ac7e42892ac74bb727b0569
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-27 22:57:47 +0800

    Removed usage of custom HTML elements

--- shrines/starwarskotor/components/sidebar-content.js
+++ shrines/starwarskotor/components/sidebar-content.js
@@ -1,14 +1,7 @@
-class RightSidebarContent extends HTMLElement {
-    constructor() {
-        super();
-    }
-    
-    connectedCallback() {
-        this.innerHTML = `
-            <h2>About</h2>
-            <p><em>Star Wars: Knights of the Old Republic</em> is a space opera role-playing video game series that takes place in the <em>Star Wars</em> universe. The story takes place approximately 4,000 years before the rise of the Galactic Empire.</p>
-        `;
-    }
-}
-  
-customElements.define('right-sidebar-component', RightSidebarContent);
+// Sidebar content
+document.querySelector(".right-sidebar").innerHTML = `
+    <div class="sidebar--sticky">
+        <h2>About</h2>
+        <p><em>Star Wars: Knights of the Old Republic</em> is a space opera role-playing video game series that takes place in the <em>Star Wars</em> universe. The story takes place approximately 4,000 years before the rise of the Galactic Empire.</p>
+    </div>
+`;
