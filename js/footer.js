218655e159f89ff2b7cc47002e2f82503c056934
Leilukin <contact@leilukin.com>
2023-08-13 19:57:38 +0800

    Streamlined JavaScript code for website components

--- js/footer.js
+++
@@ -1,6 +0,0 @@
-// Footer component content
-document.querySelector("footer").innerHTML = `
-    <p>Made with ♥ by Leilukin | <a href="/sitemap.html">Site Map</a></p>
-    <p>Site Launched: September 11, 2022 | <a href="/changelog">Website Changelog</a></p>
-    <p><a href="#page-top">↑ Back to top ↑</a></p>
-`;
