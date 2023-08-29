ff4d8812647927e0fd0cc9ab41da6d6445aca8f3
Leilukin <contact@leilukin.com>
2023-08-29 22:14:04 +0800

    Use Font Awesome for arrows

--- js/main-components.js
+++ js/main-components.js
@@ -41,5 +41,9 @@ window.addEventListener("scroll", e => {
 document.querySelector(".main-footer").innerHTML = `
     <p>Made with ♥ by Leilukin | <a href="/sitemap.html">Site Map</a></p>
     <p>Site Launched: September 11, 2022 | <a href="/changelog">Website Changelog</a></p>
-    <p><a href="#page-top">↑ Back to top ↑</a></p>
+    <p>
+        <a href="#page-top">
+            <i class="fa fa-arrow-up"></i> Back to top <i class="fa fa-arrow-up"></i>
+        </a>
+    </p>
 `;
