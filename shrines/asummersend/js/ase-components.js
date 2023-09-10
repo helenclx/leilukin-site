20f048d456d518840bc074d9f7fe76943b3d31eb
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-09-10 23:30:05 +0800

    Modify right sidebar content

--- shrines/asummersend/js/ase-components.js
+++ shrines/asummersend/js/ase-components.js
@@ -34,7 +34,7 @@ window.addEventListener("scroll", e => {
 document.querySelector(".right-sidebar").innerHTML = `
     <div class="sidebar--sticky">
         <h2>About</h2>
-        <p><cite>A Summer’s End — Hong Kong 1986</cite> is a visual novel set in 1980s Hong Kong. It tells of a love story between two women. Lush and vibrant visuals, the game features over 400 unique hand-drawn artwork inspired by Asian cinema and 1980s anime.</p>
+        <p>Welcome to Leilukin's shrine for <cite>A Summer’s End — Hong Kong 1986</cite>, a visual novel developed by Oracle and Bone. Set in 1980s Hong Kong, it tells of a love story between two women.</p>
         <h3>Links</h3>
         <ul>
             <li><a href="https://www.asummersend.com/" target="_blank">Official Website</a>
