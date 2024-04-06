f838ada8aee90b80142bd3a011a04c141e0972c1
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-07 01:15:00 +0800

    Output the feed file to the root of the generated site

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
+    eleventyConfig.addPassthroughCopy({ "./src/assets/feed/": "/" });
 
     return {
         dir: {
