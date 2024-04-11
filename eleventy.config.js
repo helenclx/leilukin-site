d55626b188a5bfa383d833d1866f7f562da4551e
Leilukin <contact@leilukin.com>
2024-04-11 20:16:05 +0800

    Install WebC plug-in

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,14 @@
+// Installed Plug-ins
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
 module.exports = function (eleventyConfig) {
+    // Copy files
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addPassthroughCopy({ "./src/assets/feed/": "/" });
+
+    // Installed Plug-ins
+    eleventyConfig.addPlugin(pluginWebc);
 
     return {
         dir: {
