4d23d73d1457b617442de93ab4e7142d038c3426
Leilukin <contact@leilukin.com>
2024-04-12 01:15:27 +0800

    Install Eleventy ToC plug-in

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const metagen = require('eleventy-plugin-metagen');
 const anchors_plugin = require('@orchidjs/eleventy-plugin-ids');
+const pluginTOC = require('eleventy-plugin-toc');
 
 module.exports = function (eleventyConfig) {
     // Copy files
@@ -21,6 +22,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(anchors_plugin, {
         selectors: ['h2','h3','h4','h5','h6'],
     });
+    eleventyConfig.addPlugin(pluginTOC);
 
     return {
         dir: {
