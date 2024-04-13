8f34daadfb1c2dfc5b466ff4bf47d630794ad2ec
Leilukin <contact@leilukin.com>
2024-04-13 15:23:57 +0800

    Add site metadata file

---
+++ src/_data/sitemeta.js
@@ -0,0 +1,11 @@
+module.exports = {
+	siteName: "Leilukin's Hub",
+	siteUrl: "https://leilukin.neocities.org/" || "http://localhost:8080",
+	siteLanguage: "en",
+	siteDescription: "Leilukin's personal website.",
+	siteAuthor: {
+		name: "Leilukin",
+		email: "leilukin@outlook.com",
+		url: "https://leilukin.neocities.org/about/"
+	}
+}
