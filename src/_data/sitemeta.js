3ed3a367fe3bbc88d37d92d5cf7149e99f6c9c85
Leilukin <contact@leilukin.com>
2024-04-13 15:43:53 +0800

    Change site locale to en_MY

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteName: "Leilukin's Hub",
 	siteUrl: "https://leilukin.neocities.org/" || "http://localhost:8080",
-	siteLanguage: "en",
+	siteLocale: "en_MY",
 	siteDescription: "Leilukin's personal website.",
 	siteAuthor: {
 		name: "Leilukin",
