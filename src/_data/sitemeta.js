4f25da2946a256f61e28fa77d2693aabbb3384c7
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-14 14:49:04 +0800

    Adjust metadata URLs

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,11 +1,11 @@
 module.exports = {
 	siteName: "Leilukin's Hub",
-	siteUrl: "https://leilukin.neocities.org/" || "http://localhost:8080",
+	siteUrl: "https://leilukin.neocities.org" || "http://localhost:8080",
 	siteLocale: "en_MY",
 	siteDescription: "Leilukin's personal website.",
 	siteAuthor: {
 		name: "Leilukin",
 		email: "leilukin@outlook.com",
-		url: "https://leilukin.neocities.org/about/"
+		url: "https://leilukin.neocities.org/about"
 	}
 }
