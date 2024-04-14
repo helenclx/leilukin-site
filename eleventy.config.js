c4281483f44153d2a4169fbba59763abb39292f5
Leilukin <contact@leilukin.com>
2024-04-15 00:56:45 +0800

    Uninstall WebC plug-in

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,5 @@
 // Installed Plug-ins
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const metagen = require('eleventy-plugin-metagen');
 const pluginTOC = require('eleventy-plugin-nesting-toc');
@@ -37,9 +36,6 @@ module.exports = function (eleventyConfig) {
 
     // Installed Plug-ins
     eleventyConfig.addPlugin(EleventyRenderPlugin);
-    eleventyConfig.addPlugin(pluginWebc, {
-        components: "./src/_includes/**/*.webc",
-    });
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(metagen);
     eleventyConfig.addPlugin(pluginTOC, { tags: ['h2', 'h3', 'h4', 'h5'] });
