7191d06324034e4ad84b5d5d5dc3a34d154c9f30
Leilukin <contact@leilukin.com>
2024-04-16 23:05:44 +0800

    Use front matter for feed URL

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,7 +1,6 @@
 module.exports = {
 	siteName: "Leilukin's Hub",
 	siteUrl: "https://leilukin.neocities.org" || "http://localhost:8080",
-	feedUrl: this.siteUrl + "/feed.xml",
 	siteLocale: "en_MY",
 	siteLanguage: "en",
 	siteDescription: "Leilukin's personal website.",
