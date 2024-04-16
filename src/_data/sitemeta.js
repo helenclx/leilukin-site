3be5a148373ada7b54383412eaa7aa74b92f1da2
Leilukin <contact@leilukin.com>
2024-04-16 20:38:14 +0800

    Add site language data

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -2,6 +2,7 @@ module.exports = {
 	siteName: "Leilukin's Hub",
 	siteUrl: "https://leilukin.neocities.org" || "http://localhost:8080",
 	siteLocale: "en_MY",
+	siteLanguage: "en",
 	siteDescription: "Leilukin's personal website.",
 	siteAuthor: {
 		name: "Leilukin",
