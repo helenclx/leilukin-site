3b72dae9b8b18b72c4c141b049c2679593d4a945
Leilukin <contact@leilukin.com>
2024-10-09 02:32:15 +0800

    Remove metagen plugin

--- src/shrines/starwarskotor/resources/resources.11tydata.js
+++ src/shrines/starwarskotor/resources/resources.11tydata.js
@@ -2,6 +2,7 @@ export default {
     tags: ["contents", "kotor resources"],
     layout: "starwarskotor/content",
     hasBreadcrumbs: true,
+    articleElement: true,
     eleventyComputed: {
         title: "{{ pageTitle }} | Resources",
         eleventyNavigation: {
