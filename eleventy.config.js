25881f607277b570a5fd2c19facfad268aae198f
Leilukin <contact@leilukin.com>
2025-05-10 09:04:35 +0800

    Remove lite YouTube embed config

--- eleventy.config.js
+++ eleventy.config.js
@@ -25,7 +25,6 @@ export default function(eleventyConfig) {
         youtube: {
             options: {
                 lazy: true,
-                lite: true,
                 recommendSelfOnly: true,
             }
         }
